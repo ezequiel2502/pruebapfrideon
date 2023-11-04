6ce3438c8c29f0935f50043335bdbefdbb6e3811
Leonardo <leo.em09@gmail.com>
2023-11-03 23:04:28 -0300

    Agrego todo lo necesario para la creacion del pdf del reporte del evento en SingleReporteActivity.java

--- app/src/main/java/com/example/sesionconfirebase/SingleReporteActivity.java
+++ app/src/main/java/com/example/sesionconfirebase/SingleReporteActivity.java
@@ -1,19 +1,23 @@
 package com.example.sesionconfirebase;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.example.sesionconfirebase.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.FileProvider;
+
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -24,14 +28,26 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+//import com.itextpdf.kernel.pdf.PdfDocument;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import android.graphics.pdf.PdfDocument;
+import android.widget.Toast;
+
 
 public class SingleReporteActivity extends AppCompatActivity {
 
     private String eventoId;
+    LinearLayout llContenedorEstadisticas;
+    CardView cardViewImpresion;
+    private static final int REQUEST_CODE_SAVE_PDF = 1;
+    private PdfDocument document;
 
 
     @Override
@@ -40,15 +56,17 @@ public class SingleReporteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_single_reporte);
 
         // Obtener el contenedor
-        LinearLayout llContenedorEstadisticas = findViewById(R.id.llContenedorEstadisticas);
+        llContenedorEstadisticas = findViewById(R.id.llContenedorEstadisticas);
 
+        //Obtengo el boton de impresion
+        cardViewImpresion=findViewById(R.id.cardViewImpresion);
 
         // Obtener el eventoId del Intent
         Intent intent = getIntent();
 
 
 
-
+        //Busco en la base de datos el reporte usando el EventoId
         if (intent != null) {
             eventoId = intent.getStringExtra("EventoId");
             if (eventoId != null) {
@@ -90,7 +108,7 @@ public class SingleReporteActivity extends AppCompatActivity {
                                         TextView tvVelocidad = itemView.findViewById(R.id.tvVelocidad);
 
                                         // Configurar los valores de los TextView con los datos de la estadística
-                                        tvPosicion.setText((i + 1)); // La posición es 1-indexed
+                                        tvPosicion.setText(String.valueOf(i + 1)); // La posición es 1-indexed
                                         tvNombreCompetidor.setText( estadistica.getUserName());
                                         tvDistancia.setText(estadistica.getDistanciaRecorrida());
                                         tvVelocidad.setText(estadistica.getVelocidadPromEvento());
@@ -156,7 +174,70 @@ public class SingleReporteActivity extends AppCompatActivity {
 
         }
 
-        }//fin OnCreate
+
+        //Boton para imprimir el reporte
+        cardViewImpresion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createPdf();
+            }
+        });
 
 
-    }//fin App
+
+
+    }//fin OnCreate
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_SAVE_PDF && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            if (uri != null) {
+                try {
+                    OutputStream outputStream = getContentResolver().openOutputStream(uri);
+                    document.writeTo(outputStream);
+                    document.close();
+                    outputStream.close();
+                    Toast.makeText(this, "Documento PDF creado y guardado con éxito", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Error al guardar el PDF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void createPdf() {
+        // Crear un nuevo documento PDF
+        document = new PdfDocument();
+
+        // Configurar el tamaño de la página
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(llContenedorEstadisticas.getWidth(), llContenedorEstadisticas.getHeight(), 1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        // Dibujar la vista en la página PDF
+        llContenedorEstadisticas.draw(page.getCanvas());
+
+        // Terminar la página
+        document.finishPage(page);
+
+        // Crear un Intent para la acción de guardar
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+        intent.putExtra(Intent.EXTRA_TITLE, "reporte.pdf");
+
+        startActivityForResult(intent, REQUEST_CODE_SAVE_PDF);
+    }
+
+
+
+
+
+
+
+}//fin App
