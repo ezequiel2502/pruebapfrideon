cead145834ef0e81d666fc4b22ec30544b5bf6e1
JuanCarlos <juanka913@gmail.com>
2023-11-13 23:55:59 -0300

    Multiples cambios

--- app/src/main/java/com/example/sesionconfirebase/ListaEventoPostulados.java
+++ app/src/main/java/com/example/sesionconfirebase/ListaEventoPostulados.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -162,26 +163,51 @@ public class ListaEventoPostulados extends AppCompatActivity {
                         eventosPublicosRef.child(idEventoPostulado).addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot eventoSnapshot) {
+
                                 if (eventoSnapshot.exists()) {
-                                    DatabaseReference eventoCompletadoRef = firebaseDatabase.getReference().child("Eventos").child("Completados").child(idEventoPostulado);
-                                    eventoCompletadoRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                            if (!snapshot.exists()) {
-                                                ModelEvento evento = eventoSnapshot.getValue(ModelEvento.class);
-                                                // Agregar el evento a la lista
-                                                recycleList.add(evento);
-                                                recyclerAdapter.notifyDataSetChanged();
-                                            }
+                                ModelEvento evento = eventoSnapshot.getValue(ModelEvento.class);
+                                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                                FirebaseUser currentUser = mAuth.getCurrentUser();
+                                DatabaseReference eventoCompletadoRef = firebaseDatabase.getReference().child("Events_Data").child(currentUser.getUid()).child(evento.getIdEvento());
+                                eventoCompletadoRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        if (!snapshot.exists()) {
+                                            ModelEvento evento = eventoSnapshot.getValue(ModelEvento.class);
+                                            // Agregar el evento a la lista
+                                            recycleList.add(evento);
+                                            recyclerAdapter.notifyDataSetChanged();
                                         }
+                                    }
 
-                                        @Override
-                                        public void onCancelled(@NonNull DatabaseError error) {
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
 
-                                        }
-                                    });
+                                    }
+                                });
+
 
                                 }
+//                                if (eventoSnapshot.exists()) {
+//                                    DatabaseReference eventoCompletadoRef = firebaseDatabase.getReference().child("Eventos").child("Completados").child(idEventoPostulado);
+//                                    eventoCompletadoRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//                                        @Override
+//                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                                            if (!snapshot.exists()) {
+//                                                ModelEvento evento = eventoSnapshot.getValue(ModelEvento.class);
+//                                                // Agregar el evento a la lista
+//                                                recycleList.add(evento);
+//                                                recyclerAdapter.notifyDataSetChanged();
+//                                            }
+//                                        }
+//
+//                                        @Override
+//                                        public void onCancelled(@NonNull DatabaseError error) {
+//
+//                                        }
+//                                    });
+//
+//                                }
                             }
 
                             @Override
